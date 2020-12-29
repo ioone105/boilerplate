d4cf36e580da65822ffa72627f26f032e61ab434
jg <ioone@hanmail.net>
2020-12-29 22:13:22 +0900

    비밀정보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI
+  // heroku에서 이름하고 똑같이 해주면 됨 (MONGO_URI)
+}
--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://weknow:abcd1234@cluster0.utlbg.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+const config = require('./config/key')
+
+const {User} = require("./models/User");
+
+// aplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+// appliction/json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하쇼')
+  res.send('Hello World! 안녕하쇼 잘되라12')
+})
+
+app.post('/resister', (req, res) => {
+
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것을 데이터 베이스에 넣어준다
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
